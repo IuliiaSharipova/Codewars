2e7f2bb4400bb2a9e1de40de1005231d43e83b25
Iuliia Sharipova <sharipova.y.s@gmail.com>
2022-06-12 21:17:55 +0200

    add new solved tasks

---
+++ 01.js
@@ -0,0 +1,31 @@
+// Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+// Examples
+// "4556364607935616" --> "############5616"
+//      "64607935616" -->      "#######5616"
+//                "1" -->                "1"
+//                 "" -->                 ""
+
+// // "What was the name of your first pet?"
+
+// "Skippy" --> "##ippy"
+
+// "Nananananananananananananananana Batman!"
+// -->
+// "####################################man!"
+
+// return masked string
+function maskify(cc) {
+	const changedStr = cc.replace(cc.substring(0, cc.length - 4), '#'.repeat(cc.substring(0, cc.length - 4).length));
+	return changedStr;
+}
+//test result
+describe('maskify', function() {
+	it('should work for some examples', function() {
+		Test.assertEquals(maskify('4556364607935616'), '############5616');
+		Test.assertEquals(maskify('1'), '1');
+		Test.assertEquals(maskify('11111'), '#1111');
+	});
+});
