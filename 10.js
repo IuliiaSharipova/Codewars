98359027a3b900b78ec956d49dd5fb7e3cc30e5f
Iuliia Sharipova <sharipova.y.s@gmail.com>
2022-06-13 21:11:45 +0200

    add new solved task

---
+++ 10.js
@@ -0,0 +1,29 @@
+// Write a function to split a string and convert it into an array
+// of words.
+
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+function stringToArray(string) {
+	return string.split(' ');
+}
+
+//Test result:
+const chai = require('chai');
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe('Basic tests', () => {
+	it('Testing for fixed tests', () => {
+		assert.deepEqual(stringToArray('Robin Singh'), [ 'Robin', 'Singh' ]);
+		assert.deepEqual(stringToArray('I love arrays they are my favorite'), [
+			'I',
+			'love',
+			'arrays',
+			'they',
+			'are',
+			'my',
+			'favorite'
+		]);
+	});
+});
