8d6123c1c956df47c720deac5f02046673a7d991
Iuliia Sharipova <sharipova.y.s@gmail.com>
2022-06-14 14:06:37 +0200

    add new solved task

---
+++ 23.js
@@ -0,0 +1,11 @@
+// // Now you have to write a function that takes an argument and
+//  returns the square of it.
+
+const square = (a) => a * a;
+
+//Test:
+describe('Tests', () => {
+	it('test', () => {
+		Test.assertEquals(square(3), 9);
+	});
+});
