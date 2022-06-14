56241058d08ecdd8604331fabd37092185d7ff93
Iuliia Sharipova <sharipova.y.s@gmail.com>
2022-06-14 13:06:42 +0200

    add new solved task

---
+++ 21.js
@@ -0,0 +1,36 @@
+// Take an array and remove every second element from the array.
+// Always keep the first element and start removing with the next element.
+
+// Example:
+
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+// None of the arrays will be empty, so you don't have to worry about that!
+
+function removeEveryOther(arr) {
+	let fullArr = [ arr[0] ];
+	for (let i = 1; i < arr.length; i++) {
+		if (i % 2 == 0) {
+			const newArr = fullArr.push(arr[i]);
+		}
+	}
+	return fullArr;
+}
+//OR:
+function removeEveryOther(arr) {
+	return arr.filter((a, i) => !(i % 2));
+}
+
+//Test:
+const chai = require('chai');
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe('Basic tests', () => {
+	it('Testing for fixed tests', () => {
+		assert.deepEqual(removeEveryOther([ 'Hello', 'Goodbye', 'Hello Again' ]), [ 'Hello', 'Hello Again' ]);
+		assert.deepEqual(removeEveryOther([ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]), [ 1, 3, 5, 7, 9 ]);
+		assert.deepEqual(removeEveryOther([ [ 1, 2 ] ]), [ [ 1, 2 ] ]);
+		assert.deepEqual(removeEveryOther([ [ 'Goodbye' ], { Great: 'Job' } ]), [ [ 'Goodbye' ] ]);
+	});
+});
