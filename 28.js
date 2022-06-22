39a8dbfdf50a288db0b202921e5fa46aee9468d6
Iuliia Sharipova <sharipova.y.s@gmail.com>
2022-06-22 12:49:08 +0200

    fixed solution

--- 28.js
+++ 28.js
@@ -5,13 +5,7 @@ Given a number n, return the number of positive odd numbers below n, EASY!
 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])*/
 function oddCount(n){
-    let arr=[];
-    for (let i=1; i<n; i++){
-        if (i%2!=0){
-            arr.push(i);
-        }
-    }
-    return arr.length;// your code here
+    return Math.floor(n/2);
 }
 /*Test:*/
 const chai = require("chai");
